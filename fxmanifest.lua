416884f5a88ca34afe7f967286f9d47d0bdb2864
Wex Scrips <wexscripts@gmail.com>
2025-03-15 10:14:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'rdr3', 'gta5' }
 lua54 'yes'
 author 'tobix'
 description 'easy eletrician job'
-version '1.0.0'
+version '1.0.1'
 
 files {
     'locales/*.json'
@@ -20,3 +20,4 @@ client_scripts {
     'client/*.lua'
 }
 
+dependency 'mx_fixwiring'
